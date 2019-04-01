e27d7035e4692558e59209e7b0d2ded10b0cc6e0
Joe Hellerstein <jlheller@uw.edu>
2019-04-01 12:51:14 -0700

    Implement persister for persisting objects using pickle.

---
+++ common_python/constants.py
@@ -0,0 +1,6 @@
+"""Constants used in common_python."""
+
+import os
+
+CODE_DIR = os.path.dirname(os.path.abspath(__file__))
+TEST_DIR = os.path.join(CODE_DIR, "tests")
---
+++ common_python/tests/util/test_persister.py
@@ -0,0 +1,39 @@
+"""Tests for Persister."""
+
+from common_python.util.persister import Persister
+from common_python import constants as cn
+
+import os
+import unittest
+
+IGNORE_TEST = False
+TEST_PATH = os.path.join(cn.TEST_DIR, "test_persister.pcl")
+OBJECT = [0, 1, 2]
+
+
+class TestPersister(unittest.TestCase):
+
+  def setUp(self):
+    self.persister = Persister(TEST_PATH)
+
+  def tearDown(self):
+    if os.path.isfile(TEST_PATH):
+      os.remove(TEST_PATH)
+
+  def testConstructor(self):
+    self.assertEqual(self.persister._path, TEST_PATH)
+
+  def testIsExist(self):
+    self.assertFalse(self.persister.isExist())
+    self.persister.set(OBJECT)
+    self.assertTrue(self.persister.isExist())
+
+  def testGetAndSet(self):
+    self.persister.set(OBJECT)
+    an_object = self.persister.get()
+    trues = [x == y for x, y in zip(an_object, OBJECT)]
+    self.assertTrue(all(trues))
+   
+
+if __name__ == '__main__':
+    unittest.main()
