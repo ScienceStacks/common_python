caa6a5ad2772879002b26f8a6ec062f85a4a3cc6
Joe Hellerstein <jlheller@uw.edu>
2019-04-01 17:59:16 -0700

    TermAnalyzer

--- common_python/constants.py
+++ common_python/constants.py
@@ -4,3 +4,8 @@ import os
 
 CODE_DIR = os.path.dirname(os.path.abspath(__file__))
 TEST_DIR = os.path.join(CODE_DIR, "tests")
+
+# Column names
+COUNT = "count"  # A count
+FRAC = "frac"  # A fractiona
+VALUE = "value"  # General value
