400d220b9c62939ac5e9bb126750981dcdac2472
Joe Hellerstein <jlheller@uw.edu>
2019-11-19 09:07:50 -0800

    Refined tests for gene_network.

--- common_python/tellurium/gene_network.py
+++ common_python/tellurium/gene_network.py
@@ -9,7 +9,10 @@ three cases.
 
 where: 
   g is the gene number
-  i indicates the kind of integration: A for and O for or
+  i indicates the kind of integration:
+      A for AND with non-competitive binding (impossible to have AND if competitive)
+      O for OR with non-competitive binding
+      P for OR with competitive binding
   s is either "+" or "-" to indicate that the protein activates
     or inhibits the gene product
   p is a protein number
@@ -37,22 +40,29 @@ import pandas as pd
 FILE_HEAD = "model_head.txt"
 FILE_TAIL = "model_tail.txt"
 CUR_DIR = os.path.dirname(os.path.abspath(__file__))
-PATH_HEAD = os.path.join(FILE_HEAD, CUR_DIR)
-PATH_TAIL = os.path.join(FILE_TAIL, CUR_DIR)
-NGENE = 8  # Number of genes
+PATH_HEAD = os.path.join(CUR_DIR, FILE_HEAD)
+PATH_TAIL = os.path.join(CUR_DIR, FILE_TAIL)
+NUM_GENE = 8  # Number of genes
+PLUS = "+"
 # Initial network from the modeling game
-INITIAL_NETWORK = ["1+0O+4", "2+4", "3+6", "4-2", "6+7A-1", "8-1"]
+INITIAL_NETWORK = [
+    "1+0O+4", "2+4", "3+6", "4-2", 5, "6+7P-1", 7, "8-1"]
 # Structure of gene string descriptor
 POS_GENE = 0
 POS_SIGN_1 = 1
 POS_PROTEIN_1 = 2
 
 # Initial arcs in reaction network
-INITIAL_NETWORK = [
-    ]
 P0 = "INPUT"  # Protein 0 is the input
 IDX_L = 0
 IDX_DMRNA = 1
+
+# Indexed by (is_competitive, is_or_integration)
+CONJUNCTION_DICT = {
+  (True, True): "P",
+  (False, True): "O",
+  (False, False): "A",
+  }
 
 
 ################ Helper Functions ###################
@@ -66,6 +76,10 @@ def _equals(list1, list2):
   diff = set(list1).symmetric_difference(list2)
   return len(diff) == 0
 
+def _extendUnique(list1, list2):
+  list1.extend(list2)
+  return list(set(list1))
+
 
 ################ Classes ###################
 class GeneDescriptor(object):
@@ -73,17 +87,20 @@ class GeneDescriptor(object):
   # and how they affect the gene's activation
 
   def __init__(self, ngene, nprots=None, 
-      is_activates=None, is_or_integration=True):
+      is_activates=None, is_or_integration=True,
+      is_competitive=False):
     """
     :param int ngene: number of the gene
     :param list-int nprots: list of protein TFs
     :param list-bool is_activates: list of how corresponding prot impacts gene
     :param bool is_or_integration: logic used to combine terms
+    :param bool is_competitive binding:
     """
     self.ngene = int(ngene)
     self.nprots = [int(p) for p in _setList(nprots)]
     self.is_activates = _setList(is_activates)
     self.is_or_integration = is_or_integration
+    self.is_competitive = is_competitive
 
   def equals(self, other):
     result = True
@@ -94,7 +111,7 @@ class GeneDescriptor(object):
         self.is_or_integration == other.is_or_integration)
     return result
 
-  def __str__(self):
+  def __repr__(self):
     def makeTerm(is_activate, nprot):
       if is_activate:
         sign = "+"
@@ -107,10 +124,8 @@ class GeneDescriptor(object):
     if len(self.nprots) > 0:
       stg += makeTerm(self.is_activates[0], self.nprots[0])
     if len(self.nprots) == 2:
-      if self.is_or_integration:
-        conjunction = "O"
-      else:
-        conjunction = "A"
+      conjunction = CONJUNCTION_DICT[
+          (self.is_competitive, self.is_or_integration)]
       stg += conjunction
       stg += makeTerm(self.is_activates[1], self.nprots[1])
     return stg
@@ -129,6 +144,7 @@ class GeneDescriptor(object):
     nprots = []
     is_activates = []
     is_or_integration = True
+    is_competitive = True
     #
     def extractTF(stg):
       if stg[0] == "+":
@@ -143,61 +159,80 @@ class GeneDescriptor(object):
     if len(string) >= 3:
       extractTF(string[1:3])
     if len(string) == 6:
-      if string[4] == "O":
+      conjunction_term = string[3]
+      if not conjunction_term in CONJUNCTION_DICT.values():
+        raise ValueError("Invalid integration term in descriptor: %s" % string)
+      if (conjunction_term == "O") or (conjunction_term == "P"):
         is_or_integration = True
       else:
         is_or_integration = False
+      if (conjunction_term == "P"):
+        is_competitive = True
+      else:
+        is_competitive = False
       extractTF(string[4:6])
     #
     return GeneDescriptor(
         ngene,
         is_or_integration=is_or_integration,
         nprots=nprots,
-        is_activates=is_activates)
+        is_activates=is_activates,
+        is_competitive=is_competitive)
 
 
 ######################################################
 class GeneReaction(object):
   """Creates the reaction for gene production of mRNA."""
     
-  def __init__(self, ngene, is_or_integration=True):
+  def __init__(self, descriptor, is_or_integration=True):
     """
-    :param int ngene: number of the gene
+    :param int/GeneDescriptor ngene: Gene descriptor or int (if default)
     :param bool is_or_integration: logic used to combine terms
     """
-    self._ngene = ngene
-    self._is_or_integration = is_or_integration
-    self._nprots = []
-    self._is_activates = []
+    if not isinstance(descriptor, GeneDescriptor):
+      ngene = descriptor
+      descriptor = GeneDescriptor(ngene,
+          is_or_integration=is_or_integration)
+    # Public properties
+    self.descriptor = descriptor
+    self.constants = [
+        self._makeVar("L"),          # IDX_L
+        self._makeVar("d_mRNA"),     # IDX_DMRNA
+        ]
+    self.reaction = None
+    # Private
     self._k_index = 0  # Index of the K constants
     self._H = self._makeVar("H")  # H constant
     self._Vm = self._makeVar("Vm")  # H constant
     self._mrna = self._makeVar("mRNA")
-    # The following are produced when a reaction is constructed
-    self.constants = [
-        self._makeVar("L"),       # IDX_L
-        self._makeVar("d_mRNA"),  # IDX_DMRNA
-        ]
-    self.reaction = None
       
-  def addProtein(self, nprot, is_activate=True):
+  def add(self, nprot, is_activate=True):
     """
+    Adds a protein to the mRNA generation reaction for this gene.
     :param int nprot: numbers of the protein that is TF for the gene
     :param list-bool is_activation: whether the gene is activation (True)
     """
-    self._nprots.append(nprot)
-    self._is_activates.append(is_activate)
+    nprots = list(set(self.descriptor.nprots).union([nprot]))
+    is_activates = list(set(self.descriptor.is_activates).union(
+        [is_activate]))
+    self.descriptor = GeneDescriptor(
+      self.descriptor.ngene,
+      nprots=nprots,
+      is_activates=is_activates,
+      is_or_integration=self.descriptor.is_or_integration,
+      )
       
   def _makeVar(self, name):
-    return "%s%d" % (name, self._ngene)
+    return "%s%d" % (name, self.descriptor.ngene)
 
   def _makeKVar(self):
     self._k_index += 1
-    var = "K%d_%d" % (self._k_index, self._ngene)
+    var = "K%d_%d" % (self._k_index, self.descriptor.ngene)
     self.constants.append(var)
     return var
 
-  def _makePVar(self, nprot):
+  @staticmethod
+  def _makePVar(nprot):
     if nprot == 0:
       stg = "INPUT"
     else:
@@ -217,53 +252,53 @@ class GeneReaction(object):
     """
     term = "%s" % self._makeKVar()
     for nprot in nprots:
-      term = term + "*%s^%s" % (self._makePVar(nprot), self._H)
+      term = term + "*%s^%s" % (GeneReaction._makePVar(nprot),
+          self._H)
     return term
 
   def _makeTFKinetics(self):
     """
     Creates the kinetics for the transcription factors
     """
-    terms = [self._makeTerm([p]) for p in self._nprots]
-    if len(self._nprots) > 1:
-      terms.append(self._makeTerm(self._nprots))
+    terms = [self._makeTerm([p]) for p in self.descriptor.nprots]
+    if (len(self.descriptor.nprots) > 1) and (
+        not self.descriptor.is_competitive):
+      terms.append(self._makeTerm(self.descriptor.nprots))
     denominator = "1"
     for term in terms:
       denominator += " + %s" % term
     numerator = ""
-    sep = ""
-    if self._is_or_integration:
-      is_first = True
-      for is_activate, term  in zip(self._is_activates, terms[:-1]):
-        if is_first:
-          is_first = False
-        else:
-          sep = " + "
+    if self.descriptor.is_or_integration:
+      for is_activate, term  in zip(
+          self.descriptor.is_activates, terms[:-1]):
         if is_activate:
-          numerator += "%s %s" %  (sep, term)
-      # Include the joint occurrence of the TFs
-      if all(self._is_activates):
-        numerator += "%s %s" %  (sep, terms[-1])
-      # If one is a repressor, then include state of both missing
-      else:
-        numerator += "%s 1" %  sep
+          numerator += "%s %s" %  (PLUS, term)
+      if all(self.descriptor.is_activates) and (
+          not self.descriptor.is_competitive):
+        numerator += "%s %s" %  (PLUS, terms[-1])
+      # Ensure that the numerator has at least on term
       if len(numerator) == 0:
         numerator = "1"  # Ensure has at least one term
     else:  # AND integration
       numerator = "1"
-      if all(self._is_activates):
+      if all(self.descriptor.is_activates):
         numerator = terms[-1]
-      elif self._is_activates[0]:
+      elif self.descriptor.is_activates[0]:
         numerator = terms[0]
       else:
-        if len(self._is_activates) > 1:
-          if self._is_activates[1]:
+        if len(self.descriptor.is_activates) > 1:
+          if self.descriptor.is_activates[1]:
             numerator = terms[1]
-    return "%s * ( %s ) / ( %s )" % (
+    # Clean up the numerator by removing leading "+"
+    splits = numerator.split(" ")
+    if splits[0] == PLUS:
+      numerator = " ".join(splits[1:])
+    result = "%s * ( %s ) / ( %s )" % (
         self._Vm, numerator, denominator)
+    return result
   
   def _makeKinetics(self):
-    if len(self._nprots) == 0:
+    if len(self.descriptor.nprots) == 0:
       stg = self._makeBasicKinetics()
     else:
       stg = "%s + %s" % (self._makeBasicKinetics(),
@@ -274,17 +309,19 @@ class GeneReaction(object):
           ])
     return stg
 
-  def makeReaction(self):
+  def generate(self):
     """
-    Updates the reaction string, self.reaction.
+    Generates the reaction string.
+    Updates:
+      self.reaction
     """
     label = self._makeVar("J")
     self.reaction = "%s: => %s; %s" % (label, 
         self._mrna, self._makeKinetics())
     
-  def __str__(self):
+  def __repr__(self):
     if self.reaction is None:
-      self.makeReaction()
+      self.generate()
     return self.reaction
 
   @classmethod
@@ -296,13 +333,13 @@ class GeneReaction(object):
     """
     if not isinstance(descriptor, GeneDescriptor):
       descriptor = GeneDescriptor.parse(descriptor)
-    gene = GeneReaction(descriptor.ngene,
+    reaction = GeneReaction(descriptor.ngene,
         descriptor.is_or_integration)
     for nprot, is_activate in  \
         zip(descriptor.nprots, descriptor.is_activates):
-      gene.addProtein(nprot, is_activate)
-    gene.makeReaction()
-    return gene
+      reaction.add(nprot, is_activate)
+    reaction.generate()
+    return reaction
       
 
 ######################################################
@@ -321,18 +358,18 @@ class GeneNetwork(object):
     """
     :param list-str gene_descriptors: gene descriptor strings
     """
-    self._ngene = NGENE
-    self._constants = []  # Constants in the model
-    self.parameters = None  # lmfit.Parameters for model
-    self.model = None  # Model string
+    self._ngene = NUM_GENE
     self._network = {}  # Key is gene; value is GeneDescriptor
     self._constants = []  # All constants in model
-    self._initialize_constants = []  # Constants to initialize to 0
-    self.updateDescriptions(initial_network, is_initialize=False)
+    self._uninitialize_constants = []  # Constants not initialized to 0
+    self.update(initial_network, is_initialize=False)
+    # Generated outputs
+    self.parameters = None  # lmfit.Parameters for model
+    self.model = None  # Model string
 
   def update(self, strings, is_initialize=True):
     """
-    Updates the entries for descriptors.
+    Updates the gene network.
     :param list-str strings: list of gene descriptor strings
     :param bool is_intialize: constants should be initialized
     Updates
@@ -341,24 +378,26 @@ class GeneNetwork(object):
       self._network
     """
     for string in strings:
-      new_desc = GeneDescriptor.parse(string)
+      new_reaction = GeneReaction.do(string)
       # Remove constants from the old descriptor
-      if desc.ngene in self.network.keys():
-        old_desc = self.network[desc.ngene]
-        self._constants = list(set(self._constants).difference(old_desc))
-        self._initialize_constants = list(set(self._constants).difference(old_desc))
-      # Add constants for the new descriptor
-      self.network[desc.ngene] = desc
-      self._constants = self._constants.extend(desc.constants)
-      if is_initialize:
-        self._initialize_constants =  \
-            self._initialize_constants.extend(desc.initialize_constants)
+      if new_reaction.descriptor.ngene in self._network.keys():
+        old_reaction = self._network[new_reaction.descriptor.ngene]
+        difference = set(self._constants).difference(
+            old_reaction.constants)
+        self._constants = list(difference)
+      # Add the new reaction to the network
+      self._network[new_reaction.descriptor.ngene] = new_reaction
+      self._constants = _extendUnique(
+          self._constants, new_reaction.constants)
+      if not is_initialize:
+        self._uninitialize_constants = _extendUnique(
+            self._uninitialize_constants, new_reaction.constants)
     # Verify that this is a complete network
     if len(self._network.keys()) != self._ngene:
-      raise ValueError("Some key is not initialized: %s:"
+      raise RuntimeError("Some key is not initialized: %s:"
           % str(self._network.keys()))
 
-  def do(self):
+  def generate(self):
     """
     Generates an antimony model for the gene network.
     Updates
@@ -366,13 +405,20 @@ class GeneNetwork(object):
       self.parameters
     """
     # 1: Append the head of the file
-    self.model = GeneNetwork._readFile(FILE_HEAD)
+    self.model = GeneNetwork._readFile(PATH_HEAD)
     # 2: Append gene reactions
+    self.model += str(self)
     # 3: Append constant initializations
+    comment = "\n\n// Initializations for new constants\n"
+    self.model += comment
+    constants = [v for v in self._constants
+        if not v in self._uninitialize_constants]
+    statements = "\n".join(["%s = 0;" % v for v in constants])
+    self.model += statements
     # 4: Append the tail of the file
-    self.model += "\n" + GeneNetwork._readFile(FILE_HEAD)
+    self.model += "\n" + GeneNetwork._readFile(PATH_TAIL)
     # 5: Construct the lmfit.parameters for constants in the model
-    self.parameters = mg.makeParameters(self.constants)
+    self.parameters = mg.makeParameters(self._constants)
 
   def copy(self):
     """
@@ -383,9 +429,9 @@ class GeneNetwork(object):
 
   @staticmethod
   def _readFile(path):
-    with open(path) as fd:
-      result = fd.readlines(path)
+    with open(path, "r") as fd:
+      result = fd.readlines()
     return "\n".join(result)
 
-  def __str__(self):
-    return self._model
+  def __repr__(self):
+    return "\n".join([str(r) for r in self._network.values()])
--- common_python/tests/tellurium/test_gene_network.py
+++ common_python/tests/tellurium/test_gene_network.py
@@ -5,6 +5,8 @@ util.addPath("common_python",
     sub_dirs=["common_python", "tellurium"])
 
 from common_python.tellurium import gene_network
+from common_python.tellurium.gene_network import  \
+    GeneDescriptor, GeneReaction, GeneNetwork
 
 import pandas as pd
 import numpy as np
@@ -27,79 +29,116 @@ GENE_0_TF = str(NGENE)
 GENE_1_TF = "%d%s%s" % (NGENE, ACTIVATE_1, PROTEIN_1)
 GENE_2_TF = "%d%s%s%s%s%s" % (
     NGENE, ACTIVATE_1, PROTEIN_1, INTEGRATE, ACTIVATE_2, PROTEIN_2)
+NEW_NETWORK = [s.replace(str(1), str(v)) for s,v 
+    in zip(np.repeat(gene_network.INITIAL_NETWORK[0], 8),
+    range(1, gene_network.NUM_GENE+1))]
 
 
 ###########################################################
 class TestGeneDescriptor(unittest.TestCase):
 
   def setUp(self):
-    self.descriptor = gene_network.GeneDescriptor.parse(GENE_2_TF)
+    if IGNORE_TEST:
+      return
+    self._init()
+
+  def _init(self):
+    if IGNORE_TEST:
+      return
+    self.descriptor = GeneDescriptor.parse(GENE_2_TF)
 
   def testConstructor(self):
-    descriptor = gene_network.GeneDescriptor(NGENE,
+    if IGNORE_TEST:
+      return
+    descriptor = GeneDescriptor(NGENE,
         nprots=[PROTEIN_1, PROTEIN_2],
         is_activates=[True, False],
         is_or_integration=False)
     self.assertTrue(self.descriptor.equals(descriptor))
 
   def testParse(self):
-    desc = gene_network.GeneDescriptor.parse("2")
+    if IGNORE_TEST:
+      return
+    desc = GeneDescriptor.parse("2")
     self.assertEqual(desc.ngene, 2)
-    desc = gene_network.GeneDescriptor.parse(2)
+    desc = GeneDescriptor.parse(2)
     self.assertEqual(desc.ngene, 2)
-    desc = gene_network.GeneDescriptor.parse("2+3")
+    desc = GeneDescriptor.parse("2+3")
     self.assertEqual(desc.ngene, 2)
     self.assertEqual(desc.is_activates[0], True)
     self.assertEqual(desc.nprots[0], 3)
-    desc = gene_network.GeneDescriptor.parse("2-3")
+    desc = GeneDescriptor.parse("2-3")
     self.assertEqual(desc.is_activates[0], False)
     self.assertEqual(desc.nprots[0], 3)
-    desc = gene_network.GeneDescriptor.parse("2-3O+4")
+    desc = GeneDescriptor.parse("2-3O+4")
     self.assertEqual(len(desc.is_activates), 2)
     self.assertEqual(desc.is_activates[0], False)
     self.assertEqual(desc.nprots[0], 3)
     self.assertEqual(desc.is_activates[1], True)
     self.assertEqual(desc.nprots[1], 4)
+
+  def testParse2(self):
+    if IGNORE_TEST:
+      return
+    # Test by checking the we can recover the original string
+    def test(descriptor_stg):
+      descriptor = GeneDescriptor.parse(descriptor_stg)
+      stg = str(descriptor)
+      self.assertEqual(descriptor_stg, stg)
+    #
+    test("6+7P-1")
+    test("1+2O-1")
+    test("1+2P-1")
+    test("1+2A-1")
+    test("1")
+    test("1+2")
     
 
 ###########################################################
 class TestGeneReaction(unittest.TestCase):
 
   def setUp(self):
-    self.maker = gene_network.GeneReaction(NGENE)
+    if IGNORE_TEST:
+      return
+    self._init()
+
+  def _init(self, descriptor=NGENE):
+    if isinstance(descriptor, str):
+      descriptor = GeneDescriptor.parse(descriptor)
+    self.reaction = GeneReaction(descriptor)
 
   def testConstructor(self):
     if IGNORE_TEST:
       return
-    self.assertEqual(len(self.maker._nprots), 0)
+    self.assertEqual(len(self.reaction.descriptor.nprots), 0)
 
-  def _addProteins(self, maker, nprots, is_activates):
+  def _adds(self, reaction, nprots, is_activates):
     if IGNORE_TEST:
       return
     is_activates = [True, False]
-    [maker.addProtein(n, is_activate=b)
+    [reaction.add(n, is_activate=b)
         for b, n in zip(is_activates, NPROTS)]
 
   def testAddProtein(self):
     if IGNORE_TEST:
       return
-    self._addProteins(self.maker, NPROTS, IS_ACTIVATES)
-    self.assertFalse(all(self.maker._is_activates))
-    self.assertEqual(len(self.maker._is_activates),
+    self._adds(self.reaction, NPROTS, IS_ACTIVATES)
+    self.assertFalse(all(self.reaction.descriptor.is_activates))
+    self.assertEqual(len(self.reaction.descriptor.is_activates),
       len(NPROTS))
-    self.assertEqual(len(self.maker._nprots),
+    self.assertEqual(len(self.reaction.descriptor.nprots),
       len(NPROTS))
    
   def testMakePVar(self):
     if IGNORE_TEST:
       return
     nprot = 24
-    self.assertEqual("P%d" % nprot, self.maker._makePVar(nprot))
+    self.assertEqual("P%d" % nprot, self.reaction._makePVar(nprot))
 
   def testMakeBasicKinetics(self):
     if IGNORE_TEST:
       return
-    stg = self.maker._makeBasicKinetics()
+    stg = self.reaction._makeBasicKinetics()
     self.assertTrue("L" in stg)
     self.assertTrue("d_mRNA" in stg)
     self.assertTrue("*mRNA" in stg)
@@ -107,20 +146,20 @@ class TestGeneReaction(unittest.TestCase):
   def testMakeTerm(self):
     if IGNORE_TEST:
       return
-    stg = self.maker._makeTerm(NPROTS)
+    stg = self.reaction._makeTerm(NPROTS)
     self.assertTrue("K1" in stg)
     self.assertEqual(stg.count("P"), 2)
-    stg = self.maker._makeTerm(NPROTS)
+    stg = self.reaction._makeTerm(NPROTS)
     self.assertTrue("K2" in stg)
 
   def testMakeTFKinetics(self):
     if IGNORE_TEST:
       return
     def test(is_or_integration):
-      maker = gene_network.GeneReaction(NGENE,
+      reaction = GeneReaction(NGENE,
           is_or_integration=is_or_integration)
-      self._addProteins(maker, NPROTS, IS_ACTIVATES)
-      stg = maker._makeTFKinetics()
+      self._adds(reaction, NPROTS, IS_ACTIVATES)
+      stg = reaction._makeTFKinetics()
       for n in range(1, 4):
         substg = "K%d" % n
         if not is_or_integration:
@@ -131,43 +170,140 @@ class TestGeneReaction(unittest.TestCase):
     test(True)
     test(False)
 
+  def testMakeTFKinetics1(self):
+    if IGNORE_TEST:
+      return
+    FACTOR = "K3_6"
+    def test(stg, is_factor):
+      descriptor = GeneDescriptor.parse(stg)
+      self._init(descriptor)
+      kinetics = self.reaction._makeTFKinetics()
+      if is_factor:
+        self.assertTrue(FACTOR in kinetics)
+      else:
+        self.assertFalse(FACTOR in kinetics)
+    #
+    test("6+7P-1", False)
+    test("6+7O-1", True)
+
+  def testMakeTFKinetics2(self):
+    if IGNORE_TEST:
+      return
+    def makeInit(var):
+      return "%s = 0\n" % var
+    # 
+    def test(desc_stg):
+      # Verifies correct construction of kinetics expression
+      ngene = int(desc_stg[0])
+      statements = makeInit("INPUT")
+      for constant in ["Vm", "H"]:
+        statements += makeInit("%s%d" % (constant, ngene))
+      # Initialize possible constants
+      for idx in range(1, 4):
+        statements += makeInit("K%d_%d" % (idx, ngene))
+      # Initialize proteins
+      for protein in range(1, gene_network.NUM_GENE + 1):
+        statements += "P%d = 0\n" % protein
+      # Add the kinetics expression
+      self._init(desc_stg)
+      kinetics = self.reaction._makeTFKinetics()
+      statements += "_ = %s\n" % kinetics
+      try:
+        exec(statements)
+      except:
+        self.assertTrue("Bad statements: %s" % statements)
+    #
+    test("1+0O+4")
+    test("6+7P-1")
+    test("6+7O-1")
+    test("1+2O-1")
+    test("1+2P-1")
+    test("1+2A-1")
+    test("1")
+    test("1+2")
+
   def testMakeReaction(self):
     if IGNORE_TEST:
       return
-    self._addProteins(self.maker, NPROTS, IS_ACTIVATES)
-    self.maker.makeReaction()
-    self.assertEqual(len(self.maker.constants), NCONST_2_TF)
-    self.assertTrue("=>" in self.maker.reaction)
-    for constant in self.maker.constants:
-      self.assertTrue(constant in self.maker.reaction)
+    self._adds(self.reaction, NPROTS, IS_ACTIVATES)
+    self.reaction.generate()
+    self.assertEqual(len(self.reaction.constants), NCONST_2_TF)
+    self.assertTrue("=>" in self.reaction.reaction)
+    for constant in self.reaction.constants:
+      self.assertTrue(constant in self.reaction.reaction)
  
   def testDo(self):
     if IGNORE_TEST:
       return
     desc_stg = "8-1"
-    spec = gene_network.GeneReaction.do(desc_stg)
+    spec = GeneReaction.do(desc_stg)
     desc_stg = "1+0O+4"
-    maker = gene_network.GeneReaction.do(desc_stg)
-    self.assertEqual(len(maker.constants),  NCONST_2_TF)
+    reaction = GeneReaction.do(desc_stg)
+    self.assertEqual(len(reaction.constants),  NCONST_2_TF)
     desc_stg = "4-2"
-    maker = gene_network.GeneReaction.do(desc_stg)
-    self.assertEqual(len(maker.constants),  NCONST_1_TF)
+    reaction = GeneReaction.do(desc_stg)
+    self.assertEqual(len(reaction.constants),  NCONST_1_TF)
 
   def testStr(self):
     if IGNORE_TEST:
       return
     # Smoke test
-    self._addProteins(self.maker, NPROTS, IS_ACTIVATES)
-    _ = str(self.maker)
+    self._adds(self.reaction, NPROTS, IS_ACTIVATES)
+    _ = str(self.reaction)
     
 
 ###########################################################
 class TestGeneNetwork(unittest.TestCase):
 
   def setUp(self):
-    self.network = gene_network.GeneNetwork()
-    
-    
+    if IGNORE_TEST:
+      return
+    self._init()
+
+  def _init(self):
+    self.network = GeneNetwork()
+
+  def testConstructor(self):
+    if IGNORE_TEST:
+      return
+    self.assertEqual(len(self.network._network.keys()),
+        gene_network.NUM_GENE)
+
+  def testUpdate(self):
+    if IGNORE_TEST:
+      return
+    def strip(stg):
+      p = stg.index("K3_1")
+      return stg[p:]
+    #
+    self.network.update(NEW_NETWORK)
+    length1 = len(str(self.network._network[1]))
+    for reaction in self.network._network.values():
+      self.assertEqual(length1, len(str(reaction)))
+
+  def testUpdate2(self):
+    if IGNORE_TEST:
+      return
+    # Tests for constant initialization
+    network = GeneNetwork(range(1,
+        gene_network.NUM_GENE + 1))
+    network.update(gene_network.INITIAL_NETWORK)
+    difference = set(self.network._constants).difference(
+        network._constants)
+    self.assertEqual(len(difference), 0)
+    difference = set(self.network._uninitialize_constants).difference(
+        network._uninitialize_constants)
+    difference = list(difference)
+    for constant_type in ["L", "d_mRNA"]:
+      self.assertFalse(constant_type in difference)
+
+  def testGenerate(self):
+    if IGNORE_TEST:
+      return
+    self.network.update(NEW_NETWORK)
+    self.network.generate()
+    self.assertGreater(len(self.network.model), 4000)
+    self.assertGreater(len(self.network.parameters.valuesdict()), 10)
 
 
 if __name__ == '__main__':
