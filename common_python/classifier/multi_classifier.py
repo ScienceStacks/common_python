00c688b898b062a1f75e061d01970f858fe327ee
Joseph Hellerstein <jlheller@uw.edu>
2020-04-18 12:09:02 -0700

    Refactor and enhance MultiClassifier.

--- common_python/classifier/multi_classifier.py
+++ common_python/classifier/multi_classifier.py
@@ -23,15 +23,22 @@ import numpy as np
 import pandas as pd
 from sklearn import svm
 
+MAX_CORR = 0.5  # Maxium correlation with an existing feature
+
 
 ###################################################
 class FeatureHandler(object):
   """
   Default handler for selecting features for a class.
-  Provides ordered values of features.
+  FeatureHandler responsibilities
+    1. Create descending order of features for each class
+    2. Select the next best feature for forward
+       feature selection. This is done by eliminating
+       from consideration features that are too highly
+       correlated with features that are already selected.
   """
 
-  def __init__(self, df_X, ser_y):
+  def __init__(self, df_X, ser_y, max_corr=0.5):
     """
     :param pd.DataFrame df_X:
         columns: features
@@ -39,54 +46,99 @@ class FeatureHandler(object):
     :param pd.Series ser_y:
         index: instances
         values: class
+    :param float max_corr: maximum correlation
+        between a new feature an an existing feature
     """
+    # Public
     self.df_X = df_X
     self.ser_y = ser_y
+    self._classes = list(self.ser_y.uniquie())
     self.all_features = df_X.columns.tolist()
-    self._feature_dct = {}
+    # Features selected for each state
+    self.feature_dct = {c: [] for c in self._classes}
+    # Private
+    # Features in descending priority order by class
+    self._max_corr = max_corr
+    self._ordered_dct = {}
+    self._df_corr = np.corrcoef(self.df_X)
 
   @property
-  def feature_dct(self):
+  def ordered_dct(self):
     """
     Features ordered in descending priority for each class.
     :return dict:
         key: class
         value: list of features in descending order
     """
-    if len(self._feature_dct) == 0:
+    if len(self._ordered_dct) == 0:
       df_fstat = util_classifier.makeFstatDF(
           self.df_X, self.ser_y)
-      for cls in self.ser_y.unique():
+      for cls in self._classes:
         ser_fstat = df_fstat[cls]
         ser_fstat.sort_values()
-        self._feature_dct[cls] = ser_fstat.index.tolist()
-    return self._feature_dct
+        self._ordered_dct[cls] = ser_fstat.index.tolist()
+    return self._ordered_dct
 
-  def getFeatures(self, cls):
-    return self.feature_dct[cls]
-
-  def getNonFeatures(self, features):
-    return list(set(self.all_features).difference(
+  def getNonFeatures(self, cls):
+    return list(set(self.feature_dct[cls]).difference(
         features))
+
+  def setNonFeatures(self, cls):
+    """
+    Sets values of non-features to zero.
+    :return pd.DataFrame: Non-feature columns are 0
+    """
+    df_X_sub = self.df_X.copy()
+    non_features = list(set(self.feature_dct[cls]
+        ).difference(self.feature_dct[cls]))
+    df_X_sub[non_features] = 0
+    return df_X_sub
+
+  def nextFeatures(self, cls):
+    """
+    Selects the next feature to add for this class.
+    :param object cls:
+    :return str: list-feature
+    """
+    df_corr = copy.deepcopy(self.df_corr)
+    df_corr = df_corr[self.feature_dct[cls]]
+    ser_max = df_corr.max(axis=1)
+    ser_max = ser_max.apply(lambda v: np.abs(v))
+    # Choose the highest priority feature that is
+    # not highly correlated with the existing features.
+    indices = ser_max.index[ser_max < self.max_corr]
+    feature_subset = [f for f in self._ordered_dct[cls]
+        if f in ser_max[sel]]
+    if len(feature_subset) > 0:
+      self.feature_dct[cls].append(feature_subset[0])
+      return self.feature_dct[cls]
+    else:
+      return None
 
 
 class MultiClassifier(object):
  
   def __init__(self, base_clf=svm.LinearSVC(),
-        feature_handler=FeatureHandler,
-        desired_accuracy=0.9):
+        feature_handler_cls=FeatureHandler,
+        desired_accuracy=0.9,
+        **kwargs):
     """
     :param Classifier base_clf: 
     :param FeatureHandler feature_handler:
     :param float desired_accuracy: accuracy for clf
+    :param dict kwargs: arguments passed to FeatureHandler
     """
-    self.base_clf = base_clf
-    self.classes = []
-    self.all_features = None
-    self.feature_handler = None
+    # Public
     self.feature_dct = {}  # Features used for classifier
     self.clf_dct = {}  # key is cls; value is clf
     self.score_dct = {}  # key is cls; value is score
+    self.feature_handler = None
+    # Private
+    self._kwargs = kwargs
+    self._desired_accuracy = desired_accuracy
+    self.feature_handler_cls = feature_handler_cls
+    self._base_clf = base_clf
+    self._classes = []
 
   def fit(self, df_X, ser_y):
     """
@@ -100,40 +152,30 @@ class MultiClassifier(object):
         index: instances
         values: class
     :globals:
-        assigned: all_features, classes, features_policy, 
+        assigned: all_features, classes,
                   clf_dct, score_dct, feature_dct
-       
     """
-    self.feature_handler = FeatureHandler(df_X, ser_y)
-    self.classes = ser_y.unique()
-    for cls in self.classes:
+    self.feature_handler = self.feature_handler_cls(
+        df_X, ser_y, **self._kwargs)
+    self._classes = ser_y.unique()
+    for cls in self._classes:
       # Initialize for this class
-      self.clf_dct[cls] = copy.deepcopy(self.base_clf)
-      features_cls = self.feature_handler.getFeatures(cls)
+      self.clf_dct[cls] = copy.deepcopy(self._base_clf)
       ser_y_cls = util_classifier.makeOneStateSer(ser_y,
           cls)
+      last_score = 0
       # Use enough features to obtain the desired accuracy
       # This may not be possible
-      for rank in range(len(features)):
-        self.feature_dct[cls] = features_cls[0:rank+1]
-        df_X_rank = self._setFeatures(df_X, cls)
-        self.clf_dct[cls].fit(df_X_rank, ser_y_rank)
-        self.score_dct[cls] = clf.score(df_X, ser_y)
-        if self.score_dct[cls] >= self.desired_accuracy:
+      for rank in range(len(features_cls)):
+        if self.feature_handler.nextFeatures(cls) is None:
           break
-
-  def _setFeatures(self, cls):
-    """
-    Initializes the features for a class.
-    :return pd.DataFrame: Non-feature columns are 0
-    :globals:
-        read: all_features
-    """
-    non_features = self.feature_handler.getNonFeatures(
-        self.feature_dct[cls])
-    df_X_sub = self.feature_handler.df_X.copy()
-    df_X_sub[non_features] = 0
-    return df_X_sub
+        df_X_rank = self.feature_handler.setNonFeatures(cls)
+        self.clf_dct[cls].fit(df_X_rank, ser_y_cls)
+        self.score_dct[cls] = self.clf_dct[cls].score(
+            df_X, ser_y_cls)
+        if self.score_dct[cls] >= self._desired_accuracy:
+          break
+      self.clf_dct[cls] = clf
 
   def predict(self, df_X):
     """
@@ -149,9 +191,8 @@ class MultiClassifier(object):
         read: clf_dct
     """
     df_pred = pd.DataFrame()
-    for cls in self.classes:
-      df_X_sub = self._setFeatures(df_X, cls)
-      df_pred[cls] = self.clf_dct[cls].predict(df_X_sub)
+    for cls in self._classes:
+      df_pred[cls] = self.clf_dct[cls].predict(df_X)
     ser_tot = df_pred.sum(axis=1)
     df_pred = df_pred/ser_tot
     return df_pred
@@ -170,7 +211,7 @@ class MultiClassifier(object):
     score = 0
     total = 0
     df_predict = self.predict(df_X)
-    for cls in self.classes:
+    for cls in self._classes:
       sel = ser_y == cls
       df_predict_sub = df_predict.loc[sel, :]
       score += df_predict_sub[cls].sum()
--- common_python/tests/classifier/test_multi_classifier.py
+++ common_python/tests/classifier/test_multi_classifier.py
@@ -6,9 +6,10 @@ from common_python.testing import helpers
 
 import pandas as pd
 import numpy as np
+from sklearn import svm
 import unittest
 
-IGNORE_TEST = True
+IGNORE_TEST = False
 
 
 class TestFeatureHandler(unittest.TestCase):
@@ -17,7 +18,6 @@ class TestFeatureHandler(unittest.TestCase):
     self.df_X, self.ser_y = test_helpers.getDataLong()
     self.handler = multi_classifier.FeatureHandler(
         self.df_X, self.ser_y)
-    
 
   def testConstructor(self):
     if IGNORE_TEST:
@@ -26,27 +26,53 @@ class TestFeatureHandler(unittest.TestCase):
         self.handler.df_X))
 
   def testFeatureDct(self):
-    # TESTING
+    if IGNORE_TEST:
+      return
     feature_dct = self.handler.feature_dct
     diff = set(feature_dct.keys()).symmetric_difference(
         self.ser_y.unique())
     self.assertEqual(len(diff), 0)
 
   def testGetFeatures(self):
+    if IGNORE_TEST:
+      return
+    for cls in self.ser_y.index:
+      features = self.handler.getFeatures(cls)
+      diff = set(features).symmetric_difference(
+          self.df_X.columns)
+      self.assertEqual(len(diff), 0)
 
   def testGetNonFeatures(self):
-    
+    if IGNORE_TEST:
+      return
+    SIZE = 10
+    for cls in self.ser_y.unique():
+      features = self.handler.getFeatures(cls)
+      non_features = self.handler.getNonFeatures(
+          features[:SIZE])
+      diff = set(non_features).symmetric_difference(
+          features[:SIZE])
+      self.assertEqual(len(diff), len(self.df_X.columns))
 
 
-class TestMultiClassifier(unittest.TestCase):
+class TestFeatureHandler(unittest.TestCase):
   
   def setUp(self):
-    pass
+    self.df_X, self.ser_y = test_helpers.getDataLong()
+    self.clf = multi_classifier.MultiClassifier()
 
   def testConstructor(self):
     if IGNORE_TEST:
       return
+    self.assertTrue(isinstance(self.clf.base_clf,
+        svm.LinearSVC))
 
+  def testFit(self):
+    if IGNORE_TEST:
+      return
+    self.clf.fit(self.df_X, self.ser_y)
+    import pdb; pdb.set_trace()
+    
 
 if __name__ == '__main__':
   unittest.main()
