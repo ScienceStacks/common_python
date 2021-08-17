Fix bug in calculation of Binomial significance levels

common_python/classifier/case_manager.py
@@ -29,6 +29,7 @@ RANDOM_FOREST_DEFAULT_DCT = {
     "min_impurity_decrease": 0.01,
     "min_samples_leaf": 5,
     }
+TREE_UNDEFINED = -2
 
 FeatureVectorStatistic = collections.namedtuple("FeatureVectorStatistic",
     "sl cnt pos")
@@ -96,11 +97,11 @@ class CaseManager:
     self._forest = None  # Random forest used to construct cases
     self._df_case = None  # Dataframe representation of cases
     self._features = list(df_X.columns)
-    self._min_num_sample = np.log10(self._max_sl)/np.log10(binomial_prob)
     self.cases = None  # Cases constructed during by calling build
     total_sample = len(self._ser_y)
-    prior_prob = sum(self._ser_y) / total_sample
-    self._binom = BinomialDistribution(total_sample, prior_prob)
+    self._prior_prob = sum(self._ser_y) / total_sample
+    self._binom = BinomialDistribution(total_sample, self._prior_prob)
+    self._min_num_sample = np.log10(self._max_sl)/np.log10(self._prior_prob)
 
   def _getCompatibleFeatureValues(self, feature, value_ub=None):
     """
@@ -117,8 +118,11 @@ class CaseManager:
     -------
     list-float
     """
-    values = self._df_X.loc[:, feature]
-    value_sub = list(set([v for v in values if v <= value_ub]))
+    values = self._df_X.loc[:, feature].to_list()
+    if value_ub is None:
+      value_sub = values
+    else:
+      value_sub = list(set([v for v in values if v <= value_ub]))
     value_sub.sort()
     return value_sub
 
@@ -136,7 +140,7 @@ class CaseManager:
     """
     indices = set(self._df_X.index)
     for feature, value in feature_vector.dict.items():
-      these_indices = self._df_X[df_X[feature] == value].index
+      these_indices = self._df_X[self._df_X[feature] == value].index
       indices = indices.intersection(these_indices)
     return list(indices)
 
@@ -219,6 +223,7 @@ class CaseManager:
           fv_statistic = self._getFeatureVectorStatistic(new_feature_vector)
           if fv_statistic.cnt < self._min_num_sample:
             continue
+          import pdb; pdb.set_trace()
           if np.abs(fv_statistic.sl) < self._max_sl:
             # Statistically significant FeatureVector is a Case.
             new_cases.append(Case(new_feature_vector, fv_statistic,
@@ -230,7 +235,7 @@ class CaseManager:
         return new_cases
       #
       # Check for termination of this recursion
-      if dtree.tree_.feature[node] == _dtree.TREE_UNDEFINED:
+      if dtree.tree_.feature[node] == TREE_UNDEFINED:
         return []
       # Initialize this recursion
       feature_dct = {}
@@ -238,11 +243,11 @@ class CaseManager:
         feature_dct = feature_vector.dict
       # Process the node
       feature_name = self._features[dtree.tree_.feature[node]]
-      threshold = dtree.tree_.threshold_[node]
-      feature_values_all = [self._getCompatibleFeatureValues(feature_name,
-          value_ub=None)]
-      feature_values_left = [self._getCompatibleFeatureValues(feature_name,
-          value_ub=threshold)]
+      threshold = dtree.tree_.threshold[node]
+      feature_values_all = list(set(self._getCompatibleFeatureValues(
+          feature_name, value_ub=None)))
+      feature_values_left = list(set(self._getCompatibleFeatureValues(
+          feature_name, value_ub=threshold)))
       feature_values_right = list(set(feature_values_all).difference(
           feature_values_left))
       import pdb; pdb.set_trace()

common_python/statistics/binomial_distribution.py
@@ -1,5 +1,12 @@
 """Tests based on the binomial distribution."""
 
+"""
+Provides calculations for significance levels for Binomial processes.
+A positive (pos) event is the occurrence of something counted.
+A negative (neg) event is the non-occurrence. Positive plus negative
+events equals the total sample size.
+"""
+
 
 import common_python.constants as cn
 
@@ -26,56 +33,65 @@ class BinomialDistribution(object):
     # Matrix of significance levels for having at least n events
     #   row: sample_size
     #   col: number of events
-    self._sl_mat = None
+    # Matrix for positive events
+    self.pos_sl_mat = self._populateSignificanceLevels(self.binomial_prob)
+    # Matrix for negative events
+    self.neg_sl_mat = self._populateSignificanceLevels(1 - self.binomial_prob)
 
-  def _populateSignificanceLevels(self):
+  def _populateSignificanceLevels(self, prob):
     """
     Populates the signifiance level matrices.
+
+    Parameters
+    ----------
+    prob: float
+    
+    Returns
+    -------
+    matrix
+        rows are sample size
+        columns are number events
     """
     def calcTailProb(sample_count, nval):
       if nval == 0:
         return 1.0
-      return 1 - stats.binom.cdf(nval - 1, sample_count, self.binomial_prob)
+      return 1 - stats.binom.cdf(nval - 1, sample_count, prob)
     # Initialize the matrix 
     size = self.max_count + 1
     mat = np.repeat(np.nan, size*size)
-    self._sl_mat = np.reshape(mat, (size, size))
+    mat = np.reshape(mat, (size, size))
     #
     for sample_count in range(self.max_count + 1):
       for npos in range(sample_count + 1):
-        self._sl_mat[sample_count, npos] = calcTailProb(sample_count, npos)
+        mat[sample_count, npos] = calcTailProb(sample_count, npos)
+    #
+    return mat
 
-  @property
-  def sl_mat(self):
-    if self._sl_mat is None:
-      self._populateSignificanceLevels()
-    return self._sl_mat
-
-  def getSL(self, num_sample, num_event, is_two_sided=True):
+  def getSL(self, num_sample, num_pos_event, is_two_sided=True):
     """
-    Calculates the significance level of obtaining at least num_event's out of
+    Calculates the significance level of obtaining at least num_pos_event's out of
     num_sample. If the test is two sided, then it also calculates
-    the significance of num_event's or fewer. In a two sided tests, the
+    the significance of num_pos_event's or fewer. In a two sided tests, the
     smaller of the two significance levels is returned. If the smaller one
     is for "too few events", the value is negative.
 
     Parameters
     ----------
     num_sample: int
-    num_event: int
+    num_pos_event: int
     is_two_sided: bool
     
     Returns
     -------
     float
     """
-    too_many_prob = self.sl_mat[num_sample, num_event]
-    num_non_event = num_sample - num_event
-    too_few_prob = self.sl_mat[num_sample, num_non_event]
-    if too_many_prob < too_few_prob:
-      sl = too_many_prob
+    pos_sl = self.pos_sl_mat[num_sample, num_pos_event]
+    num_neg_event = num_sample - num_pos_event
+    neg_sl = self.neg_sl_mat[num_sample, num_neg_event]
+    if pos_sl < neg_sl:
+      sl = pos_sl
     else:
-      sl = -too_few_prob
+      sl = -neg_sl
     return sl
 
   def isLowSL(self, num_sample, num_event, max_sl, **kwargs):

common_python/tests/classifier/test_case_manager.py
@@ -1,6 +1,7 @@
 from common_python.classifier.feature_set  \
     import FeatureSet, FeatureVector
-from common_python.classifier.case_manager import CaseManager, Case
+from common_python.classifier.case_manager  \
+    import CaseManager, Case, FeatureVectorStatistic
 from common_python import constants as cn
 from common_python.tests.classifier import helpers as test_helpers
 
@@ -8,6 +9,7 @@ import copy
 import numpy as np
 import os
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 import unittest
 
 IGNORE_TEST = True
@@ -20,7 +22,7 @@ SER_Y = SER_Y.apply(lambda v: 1 if v == CLASS else 0)
 FEATURE_A = "Rv2009"
 FEATURE_B = "Rv3830c"
 FEATURE_SET_STG = FEATURE_A + "+" + FEATURE_B
-VALUE1 = 1
+VALUE1 = -1
 VALUE2 = -1 
 VALUES = [VALUE1, VALUE2]
 FEATURE_VECTOR = FeatureVector(
@@ -61,20 +63,22 @@ class TestCaseManager(unittest.TestCase):
     values = self.manager._getCompatibleFeatureValues(FEATURE_B, 0.5)
     self.assertEqual(values, [-1, 0])
 
-  def testGetFeatureVectors(self):
+  def testGetStatistic(self):
     if IGNORE_TEST:
       return
-    dtree = self.manager._forest.estimators_[0]
-    feature_vectors = self.manager._getFeatureVectors(dtree)
-    import pdb; pdb.set_trace()
-
-  def testGetStatistic(self):
-    # TESTING
-    statistic = self.manager.getFeatureVectorStatistic(FEATURE_VECTOR)
-    import pdb; pdb.set_trace()
+    statistic = self.manager._getFeatureVectorStatistic(FEATURE_VECTOR)
     self.assertLess(statistic.sl, 0)
     self.assertLess(np.abs(statistic.sl), 0.01)
     self.assertGreater(statistic.cnt, statistic.pos)
+
+  def testGetCases(self):
+    # TESTING
+    clf = RandomForestClassifier(max_depth=4, random_state=0, bootstrap=False,
+                                min_impurity_decrease=.01, min_samples_leaf=5)
+    _ = clf.fit(DF_X, SER_Y)
+    dtree = clf.estimators_[0]
+    feature_vectors = self.manager._getCases(dtree)
+    import pdb; pdb.set_trace()
 
 
 if __name__ == '__main__':